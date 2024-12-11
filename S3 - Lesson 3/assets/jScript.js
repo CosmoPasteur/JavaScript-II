cd9efe2578fe9c8a998a631abc87907a05cd5486
“Cosimo <pastore_cosimo@yahoo.it>
2024-12-11 16:51:03 +0100

    S3/L3

--- S3 - Lesson 3/assets/jScript.js
+++ S3 - Lesson 3/assets/jScript.js
@@ -1,13 +1,19 @@
-document.addEventListener(() => {
+document.addEventListener("",() => {
     const newTaskBtn = document.querySelector("#newTaskBtn")
     const input = document.querySelector("newTask")
 
-    newTaskBtn.addEventListener("click", ()=> {
+    newTaskBtn.addEventListener("click", ()=>{
         console.log("newTaskBtn")
     })
 
-    input.addEventListener("keypress", ()=> {
-        console.log("newTask")
+    newTask.addEventListener("keypress", (e)=>{
+        if(e.key === "Enter"){
+            newTask.value = ""
+        }
     })
 
 })
+
+const TODOList{
+    
+}
